c10b075e97fbb078522fe0414ad55d2ddb813f5c
Min.Kim <min.kim@forward3d.com>
2017-08-16 15:25:19 +0100

    Add insights api async example

--- lib/facebook_ads/ad_object.rb
+++ lib/facebook_ads/ad_object.rb
@@ -131,7 +131,6 @@ module FacebookAds
       define_method("#{verb}_edge") do |edge_name, params = {}, &block|
         params, options = extract_options(params)
         path = "#{self.id}/#{edge_name}"
-        puts path
         APIRequest.new(verb, path, session: session, params: params, options: options).execute do |api_response|
           @last_api_response = api_response
           block ? block[api_response.result] : api_response
