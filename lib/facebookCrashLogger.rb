ee4a38ae7eb16d0f8ea0836e56ffb8c20029ed0e
joesusnick <joesusnick@gmail.com>
2019-09-27 17:16:02 -0700

    Adds crash logger to correct module. Ready to  log to server.

--- lib/facebookCrashLogger.rb
+++ lib/facebookCrashLogger.rb
@@ -1,46 +1,66 @@
-class FacebookCrashLogger
-  class << self
-    private
-    @@enabled = false
-    @@hasRegisteredCrashBlock = false
+require 'facebook_ads/api_request'
+require 'facebook_ads/api_response'
 
-    # Checks if the exit was due to an exception that
-    # includes 'facebook' in the stack trace
-    def logCrash
-      if $@ != nil then
-        isFacebookCrash = $@.any? { |frame|
-          frame.match? 'facebook'
-        }
-        # TODO: Send to server
-        puts "Logging facebook sdk crash"
+module FacebookAds
+  class FacebookCrashLogger
+    class << self
+      private
+      @@enabled = false
+      @@hasRegisteredCrashBlock = false
+
+      # Checks if the exit was due to an exception that
+      # includes 'facebook' in the stack trace
+      def logCrash
+        if $@ != nil then
+          isFacebookCrash = $@.any? { |frame|
+            frame.match? 'facebook'
+          }
+
+          if isFacebookCrash
+            params = {
+              'bizsdk_crash_report': {
+                'reason': 'call fail',
+                'platform': 'platform',
+                'callstack': $@
+              }
+            }
+            APIRequest.new(
+              :post,
+              '/instruments',
+              session: Session.current_session,
+              params: params,
+              options: {} # batch options, not necessary for now
+            ).execute do |response|
+              puts response
+            end
+          end
+        end
       end
-    end
 
-    def completion
-      if @@enabled then
-        logCrash()
-      else
-        nil
+      def completion
+        if @@enabled then
+          logCrash()
+        end
       end
-    end
 
-    public
-    # Enable crash reporting
-    def enable
-      if !@@hasRegisteredCrashBlock then
-        at_exit do
-          completion()
+      public
+      # Enable crash reporting
+      def enable
+        if !@@hasRegisteredCrashBlock then
+          at_exit do
+            completion()
+          end
+
+          @@hasRegisteredCrashBlock = true
         end
 
-        @@hasRegisteredCrashBlock = true
+        @@enabled = true
       end
 
-      @@enabled = true
-    end
-
-    # Disable crash reporting
-    def disable
-      @@enabled = false
+      # Disable crash reporting
+      def disable
+        @@enabled = false
+      end
     end
   end
 end
